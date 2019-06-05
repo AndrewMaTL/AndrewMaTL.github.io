5430deba241b57eb601c1c14ba50b932f684c568
AndrewMaTL <ml4end@gmail.com>
2019-06-05 21:06:35 +0800

    home/ responsive desgin

--- js/globel.js
+++ js/globel.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
+    // copyright footer
     dt = new Date;
     $("footer").text("© Copyright " + dt.getFullYear() + " Andrew Ma");
 
+    //change website language 
     var newURL = window.location.pathname.split('/')[2];
     $(".cn").attr("href", "/cn/" + newURL);
     $(".en").attr("href", "/en/" + newURL);
+
 
 })
